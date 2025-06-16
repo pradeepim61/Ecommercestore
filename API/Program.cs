76147ae67567906c94b9316585ab94cca5b4f0c5
Pradeep kumar <pradeepim61@gmail.com>
2025-06-15 20:58:07 -0400

    Added Entity Framework DB Context and seed sample data to DB.

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,5 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
+
+DbInitializer.InitDb(app);
 
 app.Run();
